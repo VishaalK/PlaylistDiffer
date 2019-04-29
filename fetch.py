3770bd3198fcb1074d1984e9fbc49f1ab4ba4419
Vishaal Kalwani <vishaalkal@gmail.com>
2019-04-28 22:41:21 -0700

    Got the user_playlists API fetch working, want to display that in a web page now

--- fetch.py
+++ fetch.py
@@ -1,11 +1,16 @@
 import os
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # convert to environment variables
 client_credentials_manager = SpotifyClientCredentials(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'])
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print (' ', i, t['name'])
+# results = sp.search(q='weezer', limit=20)
+# for i, t in enumerate(results['tracks']['items']):
+    # print (' ', i, t['name'])
+
+playlists = sp.user_playlists('vishaalk', limit=1)
+print(json.dumps(playlists))
+# print(len(playlists))
